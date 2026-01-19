6aec9fcb4b55ecc92f1e87f85d368c4ffed46dbe
MoonLordTheSecond <moonlordthesecond@abv.bg>
2026-01-19 19:09:26 +0200

    0.00.0000

---
+++ SoundCloud_downloader.py
@@ -0,0 +1,36 @@
+import yt_dlp
+import os
+from datetime import datetime
+
+date = str(datetime.now()) # get current date, in format YYYY-MM-DD
+date = date[:10]
+
+# create folder "YYYY-MM-DD_MUSIC" in parent dir and separate folders for each playlist
+savePath: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{date}_MUSIC/%(playlist_title)s")
+
+# Final format template to import into yt-dlp
+# X_title.original-extension, X is Number of track in playlist
+outputTemplate: str = os.path.join(savePath, "%(playlist_index)s_%(title)s.%(ext)s")
+
+# Read urls.txt file , URLS are ON SEPARATE LINES
+with open( "urls.txt", "r", buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None) as urlsTxt:
+    urlsList: list[str] = urlsTxt.read().splitlines()
+
+def main():
+    ydl_options: dict = {
+        'outtmpl': {'default': outputTemplate},    #Import file formatting code in yt-dlp
+
+        'format': 'bestaudio/best',                # best audio-only
+        'continuedl': True,                        # continue download if connection breaks
+        'ignoreerrors': False,                     # ignores errors
+        'nopart': True,                            # download whole files
+        '--no-overwrites': True,                   # don't overwrite existing files
+
+        '--download-archive downloaded.txt': True  #.txt file for downloaded files
+    }
+
+    with yt_dlp.YoutubeDL(ydl_options) as ydl: # apply Yt Dlp options and download
+        ydl.download(urlsList)
+
+if __name__ == '__main__': # if script is directly run: execute script
+    main()
